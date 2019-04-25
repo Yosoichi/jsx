8359f2f6175354aa2a316d0e0af7e2c1a27cfe03
Yosoichi <49706857+Yosoichi@users.noreply.github.com>
2019-04-25 14:14:05 +0900

    Update Id_選択列のテキスト入れ替え.jsx
    条件分岐を設定

--- Id_選択列のテキスト入れ替え.jsx
+++ Id_選択列のテキスト入れ替え.jsx
@@ -1,10 +1,14 @@
 ﻿var doc = app.activeDocument;
 var sel = doc.selection[0];
-var col1=sel.columns[0];
-var col2=sel.columns[1];
-var col1C=col1.contents;
-var col2C=col2.contents;
+if((sel.columns.length>2)||(sel.columns.length<=1)){
+exit();
+}else{
+  var col1=sel.columns[0];
+  var col2=sel.columns[1];
+  var col1C=col1.contents;
+  var col2C=col2.contents;
 
-//列の文字を入れ替え
-col1.contents=col2C;
-col2.contents=col1C;
+  //列の文字を入れ替え
+  col1.contents=col2C;
+  col2.contents=col1C;
+}
