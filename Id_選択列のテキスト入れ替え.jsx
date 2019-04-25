8089ec9f571af401d4360a7a61c381be2f1005a4
Yosoichi <49706857+Yosoichi@users.noreply.github.com>
2019-04-25 14:11:43 +0900

    Update Id_選択列のテキスト入れ替え.jsx
    動くように書き直し

--- Id_選択列のテキスト入れ替え.jsx
+++ Id_選択列のテキスト入れ替え.jsx
@@ -1,3 +1,10 @@
 ﻿var doc = app.activeDocument;
-var sel = doc.selection[0].rows[0].cells;
-alert(sel.contents);
+var sel = doc.selection[0];
+var col1=sel.columns[0];
+var col2=sel.columns[1];
+var col1C=col1.contents;
+var col2C=col2.contents;
+
+//列の文字を入れ替え
+col1.contents=col2C;
+col2.contents=col1C;
