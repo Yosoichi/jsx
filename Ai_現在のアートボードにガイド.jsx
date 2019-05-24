e59f0c1cc5bcbc1f0cf59186293f4cab07aa7ac6
Yosoichi <49706857+Yosoichi@users.noreply.github.com>
2019-05-24 17:48:07 +0900

    Update Ai_現在のアートボードにガイド.jsx
    余分な行、コメントを削除

--- Ai_現在のアートボードにガイド.jsx
+++ Ai_現在のアートボードにガイド.jsx
@@ -12,83 +12,78 @@ var x2=abRect[2];
 var y2=abRect[3];
 
 //表示する単位を設定
-//単位ごとの条件分岐。除算する係数を設定
 switch(doc.rulerUnits){
-    case RulerUnits.Millimeters:
-        var unit="mm";
-    break;
-    
-    case RulerUnits.Points:
-        var unit="pt";
-    break;
-    
-    case RulerUnits.Pixels:
-        var unit="pixel";
-    break;
-    
-    case RulerUnits.Inches:
-        var unit="インチ";
-    break;
-    
-    case RulerUnits.Qs:
-        var unit="Q";
-    break;
-    
-    case RulerUnits.Picas:
-        var unit="パイカ";
-    break;
-    
-    default:
-        var unit="";
-    break;
+	case RulerUnits.Millimeters:
+		var unit="mm";
+	break;
+	
+	case RulerUnits.Points:
+		var unit="pt";
+	break;
+	
+	case RulerUnits.Pixels:
+		var unit="pixel";
+	break;
+	
+	case RulerUnits.Inches:
+		var unit="インチ";
+	break;
+	
+	case RulerUnits.Qs:
+		var unit="Q";
+	break;
+	
+	case RulerUnits.Picas:
+		var unit="パイカ";
+	break;
+	
+	default:
+		var unit="";
+	break;
 }
 
 //オフセット値を設定
 var prmpt=prompt("オフセット値を入力（単位："+unit+"）","");
 if(prmpt!=null){
-    try{
-    //全角数字対応
-    var prrep = prmpt.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
-        return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-        });
-    var offset=Number(prrep);
-    }catch(e){
-    }
+	try{
+	//全角数字対応
+	var prrep = prmpt.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+		return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+		});
+	var offset=Number(prrep);
+	}catch(e){
+	}
 }
 
 
 //単位ごとの条件分岐
 switch(doc.rulerUnits){
-    case RulerUnits.Millimeters:
-        var coefficient=0.35278;
-        var offsetInt=parseFloat(offset)/coefficient;
-    break;
-    case RulerUnits.Points:
-    case RulerUnits.Pixels:
-        var coefficient=1;
-        var offsetInt=parseFloat(offset)/coefficient;
-    break;
-    case RulerUnits.Inches:
-        var coefficient=72;
-        var offsetInt=parseFloat(offset)*coefficient;
-    break;
-    case RulerUnits.Qs:
-        var coefficient=1.41;
-        var offsetInt=parseFloat(offset)/coefficient;
-    break;
-    case RulerUnits.Picas:
-        var coefficient=12;
-        var offsetInt=parseFloat(offset)*coefficient;
-    break;
-    default:
-    //alert("");
-    break;
+	case RulerUnits.Millimeters:
+		var coefficient=0.35278;
+		var offsetInt=parseFloat(offset)/coefficient;
+	break;
+	case RulerUnits.Points:
+	case RulerUnits.Pixels:
+		var coefficient=1;
+		var offsetInt=parseFloat(offset)/coefficient;
+	break;
+	case RulerUnits.Inches:
+		var coefficient=72;
+		var offsetInt=parseFloat(offset)*coefficient;
+	break;
+	case RulerUnits.Qs:
+		var coefficient=1.41;
+		var offsetInt=parseFloat(offset)/coefficient;
+	break;
+	case RulerUnits.Picas:
+		var coefficient=12;
+		var offsetInt=parseFloat(offset)*coefficient;
+	break;
+	default:
+	break;
 }
-
-
-
 
 //長方形を作成
 var lay=doc.layers[0];
 var rect=lay.pathItems.rectangle(y1-offsetInt,x1+offsetInt,x2-x1-offsetInt*2,((y2-y1)*-1)-offsetInt*2);
-    rect.guides=true;
+	rect.guides=true;	
